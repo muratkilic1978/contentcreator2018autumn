dd556d5c3d069ca9fb80f311f3c907d3f919209f
muratkilic1978 <muratkilic1978@gmail.com>
2018-09-03 07:04:37 +0200

    Making the WordPress Social Menu Icons
    This branch (08) demonstrates how to create the Social Menu Icons in WordPress (part-1) inside the index.php file and functions.php file.

--- day4/learncustomwp/wp-content/themes/minifolio/index.php
+++ day4/learncustomwp/wp-content/themes/minifolio/index.php
@@ -181,14 +181,7 @@
     <div class="container">
       <div class="col-md-12">
         <p>
-        <ul class="footer-share">
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-          <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-          <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
-        </ul>
+        <?php wp_nav_menu( array( 'theme_location' => 'social', 'menu_class' => 'footer-share' ) ); ?>
         </p>
         <p>© 2015 All rights reserved. All Rights Reserved<br>
           Made with <i class="fa fa-heart pulse"></i> by <a href="http://www.designstub.com/">Designstub</a><br>
