235d5571a2525115d9d7c1d2a701d86354154160
muratkilic1978 <muratkilic1978@gmail.com>
2018-09-26 15:50:51 +0200

    Custom WordPress plugin partially completed 01
    Custom WordPress plugin partially completed 01.

---
+++ day5/01/idea-pro-example/idea-pro-example.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*Plugin Name: Contact Form Plugin
+*Description: This is a simple WordPress plugin
+*Author: Murat Kilic
+*Author URI: http://www.nodomain.com
+*Version: 0.1
+**/
+
+   
+?>
+
+
